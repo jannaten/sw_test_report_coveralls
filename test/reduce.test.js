50efd2b4101361f8f1a651669c5687f4f9f82198
Juhana Kivelä <juhana.kivela@tuni.fi>
2024-11-20 09:42:20 +0200

    Add reduce test cases

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -17,6 +17,23 @@ describe('reduce.js', () => {
 
             expect(result).to.equal(18);
         });
+        it('should concatenate the array elements', () => {
+            const strings = ['a', 'b', 'c'];
+
+            const result = reduce(strings, (concatenated, s) => concatenated + s, '');
+
+            expect(result).to.equal('abc');
+        });
+        it("Should create arrays of the same values", () => {
+            const objects = { 'Finland' : 3 , 'Sweden' : 1 , 'Norway' : 5, 'Denmark' : 3 };
+
+            const results = reduce(objects, (result, value, key) => {
+                (result[value] || (result[value] = [])).push(key);
+                return result;
+            }, {});
+
+            expect(results).to.eql({ '1': ['Sweden'], '3': ['Finland', 'Denmark'], '5': ['Norway'] });
+        });
     });
     describe('negative tests', () => {
         it('should return undefined for empty array', () => {
@@ -39,6 +56,33 @@ describe('reduce.js', () => {
             const result = reduce(numbers, (sum, n) => sum + n, 0);
 
             expect(result).to.equal(-4);
+        });
+        it('should handle strings with zero instances', () => {
+            const objects = { 'Finland' : 1 , 'Sweden' : 0 , 'Norway' : 0, 'Denmark' : 0 };
+
+            const results = reduce(objects, (result, value, key) => {
+                (result[value] || (result[value] = [])).push(key);
+                return result;
+            }, {});
+
+            expect(results).to.eql({ '1': ['Finland'], '0': ['Sweden', 'Norway', 'Denmark'] });
+        });
+        it('should handle strings with negative instances', () => {
+            const objects = { 'Finland' : 1 , 'Sweden' : -1 , 'Norway' : 1, 'Denmark' : -1 };
+
+            const results = reduce(objects, (result, value, key) => {
+                (result[value] || (result[value] = [])).push(key);
+                return result;
+            }, {});
+
+            expect(results).to.eql({ '1': ['Finland', 'Norway'], '-1': ['Sweden', 'Denmark'] });
+        });
+        it('should handle empty strings', () => {
+            const strings = ['', '', ''];
+
+            const result = reduce(strings, (concatenated, s) => concatenated + s, '');
+
+            expect(result).to.equal('');
         });
     });
 });
